b46d69537f8ba5f6512a5b0d2f659619d711ef96
ipelcher <53407535+ipelcher@users.noreply.github.com>
2020-02-21 10:19:07 +0300

    Delete 1.js

--- 1.js
+++
@@ -1,7 +0,0 @@
-jQuery('document').ready(function() {
-  jQuery('button').click(function() {
-    var ssid, ugu;
-    ssid = jQuery('input').val();
-    jQuery('div.popka').html('23434hee-sdfa-d3yedf93ew3--' + ssid + '--3f8adfaw7ef-adfqaawd');
-  })
-})
